3ce5dca035a5361365c8ce5f5a20e7a66d9ec9be
Marco Dizo <mdizo6482@gmail.com>
2023-04-23 04:58:32 +0800

    Convert eslint config to cjs

---
+++ astro/eslint.config.cjs
@@ -0,0 +1,34 @@
+/** @type {import("eslint").Linter.Config} */
+
+module.exports = {
+  extends: ['standard-with-typescript', 'plugin:prettier/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: true,
+    sourceType: 'module',
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    '@typescript-eslint/triple-slash-reference': 'warn'
+  },
+  overrides: [
+    // Configuration for astro files
+    {
+      files: ["**/*.astro"],
+      extends: [
+        'standard-with-typescript', 'plugin:prettier/recommended', 'plugin:astro/recommended', 'plugin:astro/jsx-a11y-recommended'
+      ],
+      // Allows Astro components to be parsed.
+      parser: "astro-eslint-parser",
+      // Parse the script in `.astro` as TypeScript
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+      rules: {
+        // override/add rules settings here, such as:
+        // "astro/no-set-html-directive": "error"
+      }
+    }
+  ]
+}
