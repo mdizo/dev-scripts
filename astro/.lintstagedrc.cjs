419960821d3d9ecea66318cdd7cf16e123fd3165
Marco Dizo <mdizo6482@gmail.com>
2023-04-23 16:11:22 +0800

    Redoing linter configs

--- astro/.lintstagedrc.cjs
+++ astro/.lintstagedrc.cjs
@@ -1,4 +1,4 @@
-export default {
+module.exports = {
     '*.{js,jsx,ts,tsx}': ['eslint --fix', 'eslint'],
     // '**/*.ts?(x)': () => 'pnpm build-types',
     '!(*.{png,jpg,jpeg,webp})': ['prettier --write --cache']
