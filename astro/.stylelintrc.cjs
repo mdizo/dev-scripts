b6f90b3abbb46cc6e6de631dec79af1befde75ac
Marco Dizo <mdizo6482@gmail.com>
2023-04-26 06:35:16 +0800

    fix(stylelint): config compatibility

--- astro/.stylelintrc.cjs
+++ astro/.stylelintrc.cjs
@@ -4,6 +4,18 @@ module.exports = {
   extends: ['stylelint-config-clean-order', 'stylelint-config-standard-scss'],
   plugins: ['stylelint-gamut'],
   rules: {
+    'declaration-empty-line-before': [
+      'always',
+      {
+        except: ['first-nested'],
+        ignore: [
+          'after-declaration',
+          'after-comment',
+          'inside-single-line-block'
+        ],
+        severity: 'warning'
+      }
+    ],
     "gamut/color-no-out-gamut-range": true,
     "function-disallowed-list": ["rgba", "hsla", "rgb", "hsl"],
     "color-function-notation": "modern",
